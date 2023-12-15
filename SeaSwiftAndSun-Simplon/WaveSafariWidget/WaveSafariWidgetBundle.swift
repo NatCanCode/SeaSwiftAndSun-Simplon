69bd6e67b001ae4363ed95eb5e5d470f05c28b68
NatCanCode <nathalie.piat1@gmail.com>
2023-12-15 11:29:27 +0100

    Add widget

---
+++ SeaSwiftAndSun-Simplon/WaveSafariWidget/WaveSafariWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  WaveSafariWidgetBundle.swift
+//  WaveSafariWidget
+//
+//  Created by N N on 15/12/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WaveSafariWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WaveSafariWidget()
+        WaveSafariWidgetLiveActivity()
+    }
+}
